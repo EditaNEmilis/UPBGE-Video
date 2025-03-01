4b53a84b17054ab554dc3234370cc2f06b3a294d
EditaNEmilis <ramanauskiene.edita@gmail.com>
2025-03-01 16:13:01 +0200

    Files added

---
+++ Video_Player/bgelogic/video.py
@@ -0,0 +1,50 @@
+# Packages
+import bge, bpy
+
+# Function
+def Main():
+
+    cont = bge.logic.getCurrentController()
+    own = cont.owner
+    
+    keyboard = bge.logic.keyboard
+    JUST_ACTIVATED = bge.logic.KX_INPUT_JUST_ACTIVATED
+    
+    # Material Data
+    bpy.data.materials["Material"].node_tree.nodes["Image Texture"].image_user.use_cyclic = True
+    bpy.data.materials["Material"].node_tree.nodes["Image Texture"].image_user.use_auto_refresh = True
+    bpy.data.materials["Material"].node_tree.nodes["Image Texture"].image_user.frame_offset = own["Offset"]
+    
+    # Clamp
+    if own["Offset"] < 0:
+        own["Offset"] = 0
+
+    ###
+    # Play/Pause
+    if JUST_ACTIVATED in keyboard.inputs[bge.events.SPACEKEY].queue:
+        if own["Playing"] == True:
+            own["Playing"] = False
+        else:
+            own["Playing"] = True
+            
+    if own["Playing"] == True and own["Reverse"] == False:
+        own["Offset"] += 1
+    else:
+        own["Offset"] = own["Offset"]
+        
+    # Reverse
+    if JUST_ACTIVATED in keyboard.inputs[bge.events.TKEY].queue:
+        if own["Reverse"] == True:
+            own["Reverse"] = False
+        else:
+            own["Reverse"] = True
+            
+    if own["Reverse"] == True:
+        own["Offset"] -= 1
+        
+    # Restart
+    if JUST_ACTIVATED in keyboard.inputs[bge.events.RKEY].queue:
+        own["Offset"] -= own["Offset"]
+    ###
+
+Main()
